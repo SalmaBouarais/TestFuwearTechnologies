96344c4acfbe43381cdc718640a5dbe9e71497d3
SalmaBouarais <45656915+SalmaBouarais@users.noreply.github.com>
2023-06-08 21:24:22 +0100

    Initial commit

---
+++ TestTech/Controllers/ActionsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestTech.Controllers
+{
+    public class ActionsController : Controller
+    {
+        [HttpGet("action")]
+        public IActionResult Action()
+        {
+            var actions = new[] { "AMZN", "CACC", "EQIX", "GOOG", "ORLY", "ULTA" };
+            var prices = new[,]
+            {
+                { 12.81, 11.09, 12.11, 10.93, 9.83, 8.14 },
+                { 10.34, 10.56, 10.14, 12.17, 13.1, 11.22 },
+                { 11.53, 10.67, 10.42, 11.88, 11.77, 10.21 },
+            };
+
+            double highestAvgPrice = 0;
+            string name = string.Empty;
+
+            for (int i = 0; i < actions.Length; i++)
+            {
+                double newAvg = 0;
+                for (int j = 0; j < prices.GetLength(0); j++)
+                {
+                    newAvg += prices[j, i];
+                }
+                newAvg /= prices.GetLength(0);
+
+                if (highestAvgPrice < newAvg)
+                {
+                    highestAvgPrice = newAvg;
+                    name = actions[i];
+                }
+            }
+
+            return Ok(new { 
+                Name = name,
+                AvgPrice = highestAvgPrice,
+            });
+        }
+    }
+}
---
+++ TestTech/Controllers/FibonacciController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestTech.Controllers
+{
+    public class FibonacciController : Controller
+    {
+        [HttpGet("fibonacci/{n}")]
+        public IActionResult FibonacciAction(int n)
+        {
+            if (n <= 1 || n >= 100)
+            {
+                return Ok(-1);
+            }
+
+            return Ok(Fibonacci(n));
+        }
+
+        private static double Fibonacci(int n)
+        {
+            double[] memo = new double[n + 1];
+            return Fibonacci(n, memo);
+        }
+
+        private static double Fibonacci(int n, double[] memo)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            else if (memo[n] > 0)
+            {
+                return memo[n];
+            }
+
+            memo[n] = Fibonacci(n - 1, memo) + Fibonacci(n - 2, memo);
+            return memo[n];
+        }
+    }
+}
